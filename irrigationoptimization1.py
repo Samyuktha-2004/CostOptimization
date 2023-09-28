1d6451983700962c6bde9526832305ac0a1bd70e
Samyuktha Boddapu <112384561+Samyuktha-2004@users.noreply.github.com>
2023-09-28 07:41:52 +0530

    Update irrigationoptimization1.py

--- irrigationoptimization1.py
+++ irrigationoptimization1.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 # Load your trained machine learning model (replace 'your_model.pkl' with your model's filename)
-with open("irrigationOptimization.pkl", "rb") as file:
+with open("your_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 # Define the Streamlit app
@@ -17,11 +18,11 @@ if st.button("Optimize"):
     # Ensure that the inputs are not empty
     if yield_input is not None and cost_input is not None:
         # Make predictions using your model
-        inputs = [[yield_input, cost_input]]
-        prediction = model.predict(inputs)[0]
+        inputs = np.array([[yield_input, cost_input]])
+        prediction = model.predict(inputs)
 
         st.header("Optimization Result:")
-        st.write(f"Predicted Yield: {prediction:.2f}")
+        st.write(f"Predicted Yield: {prediction[0]:.2f}")
 
         # Optionally, you can provide additional information or analysis here
     else:
@@ -29,3 +30,6 @@ if st.button("Optimize"):
 
 # Optionally, you can provide an explanation or guidance to the user
 
+# Debugging statements
+st.write(f"Inputs shape: {inputs.shape}")  # Debugging
+st.write(f"Model classes: {model.classes_}")  # Debugging
